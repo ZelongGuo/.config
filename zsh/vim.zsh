553e28c7613a26c162369488315260020a212ceb
zelong <zelong.guo@outlook.com>
2023-05-17 21:20:36 +0200

    update vi-mode in ternimal

--- zsh/vim.zsh
+++ zsh/vim.zsh
@@ -9,10 +9,12 @@ bindkey -M vicmd "L" vi-end-of-line
 bindkey -M vicmd "k" down-line-or-history
 bindkey -M vicmd "i" up-line-or-history
 bindkey -M vicmd "u" undo
-# #bindkey -M vicmd "-" vi-rev-repeat-search
-# bindkey -M vicmd "=" vi-repeat-search
-# bindkey -M vicmd "h" vi-forward-word-end
-# 
+# 'jk' go to VI mode
+bindkey -M viins 'jk' vi-cmd-mode
+#bindkey -M vicmd "-" vi-rev-repeat-search
+#bindkey -M vicmd "=" vi-repeat-search
+#bindkey -M vicmd "h" vi-forward-word-end
+
 # function zle-keymap-select {
 # 	if [[ ${KEYMAP} == vicmd ]] || [[ $1 = 'block' ]]; then
 # 		echo -ne '\e[1 q'
@@ -21,6 +23,7 @@ bindkey -M vicmd "u" undo
 #   fi
 # }
 # zle -N zle-keymap-select
+# 
 # 
 # # Use beam shape cursor on startup.
 # echo -ne '\e[5 q'
