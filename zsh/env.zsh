cbf10a5e6b498a23a3162d141c9e6befae026f43
zelong <zelong.guo@outlook.com>
2023-10-31 18:52:33 +0100

    add GMT settings about gmtswitch

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,9 +1,31 @@
-# -- NVIM SETTINGS --------------------------------------------------------------------
+# -------------------------------------------------------------------
+# --- GMT ---
+# NOTE: once you change the gmt version using gmtswitch, you should refresh the terminal or source ~/.zshrc to reset the GS_LIB
+# for more information, see
+# https://docs.gmt-china.org/latest/module/gmtswitch/
+# https://docs.gmt-china.org/latest/install/macOS/#gmt
+export GMTHOME=$HOME/this_gmt
+# export GMTHOME=/Applications/GMT-6.4.0.app/Contents/Resources
+export PATH=${GMTHOME}/bin:${PATH}
+export PROJ_LIB=$GMTHOME/share/proj
+GMT_VERSION=$(gmt --version)
+if [[ "$GMT_VERSION" == "6.2.0" ]]; then
+	export GS_LIB=$GMTHOME/share/ghostscript/9.53.3/Resource/Init
+else
+	export GS_LIB=$GMTHOME/share/ghostscript/Resource/Init
+fi
+export MAGICK_CONFIGURE_PATH=$GMTHOME/lib/GraphicsMagick/config
+
+
+# -------------------------------------------------------------------
+# --- NVIM SETTINGS ---
 export MYNVIMRC="~/.config/nvim/init.vim"
 # DO NOT LOAD DEFAULT RC, USING THE COUSTOM ONE
 export RANGER_LOAD_DEFAULT_RC="false"
 # change the default editor to nvim
 export EDITOR=nvim
-# conda
+
+# -------------------------------------------------------------------
+# --- CONDA ---
 export conda="/Users/zelong/opt/miniconda3/etc/profile.d/conda.sh"
 
--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -2,7 +2,7 @@ export PLUG_DIR=$HOME/.zim
 if [[ ! -d $PLUG_DIR ]]; then
     curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
     rm $HOME/.zimrc
-    ln -s $HOME/.configrc/zsh/zimrc $HOME/.zimrc
+    ln -s $HOME/.config/zsh/zimrc $HOME/.zimrc
 fi
 
 if ! cat $HOME/.zshrc | grep 'source ~/.config/zsh/zshrc' > /dev/null 2>&1; then
