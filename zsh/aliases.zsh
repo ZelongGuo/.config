2d44298041e0dbff7ec0889c0f5f2bf090462e83
zelong <zelong.guo@outlook.com>
2024-10-22 10:51:30 +0200

    update tmux settings

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,5 +4,6 @@ alias vi='nvim'
 alias ra='ranger --choosedir=$HOME/.rangerdir;LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
 alias t='tmux'
 alias tn='tmux new -s zelong'
-alias t1='tmux new -s zelong'
-alias t2='tmux new -s zelong_2'
+# avoid using the following command as it'll lead to nesting ... 1 tmux session is already enough 
+alias t1='tmux new -s 1'
+alias t2='tmux new -s 2'
