181449413dc130f6a9da8083f912dc8f0dd3bf76
zelong <zelong.guo@outlook.com>
2024-11-16 22:20:01 +0100

    add lazygit shortcuts for zsh and ranger

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,6 +3,7 @@ alias vi='nvim'
 # alias ra='ranger'
 alias ra='ranger --choosedir=$HOME/.rangerdir;LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
 alias t='tmux'
+alias g='lazygit'
 alias tn='tmux new -s zelong'
 # avoid using the following command as it'll lead to nesting ... 1 tmux session is already enough 
 alias t1='tmux new -s 1'
