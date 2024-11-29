d89381415d35b4e2bd814fde01f3f314a450eac6
zelong <zelong.guo@outlook.com>
2024-11-29 20:14:51 +0100

    python -> python3

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,3 +8,6 @@ alias tn='tmux new -s zelong'
 # avoid using the following command as it'll lead to nesting ... 1 tmux session is already enough 
 alias t1='tmux new -s 1'
 alias t2='tmux new -s 2'
+# # PYTHON
+# alias python='python3' # /opt/homebrew/bin/python3 also usr/bin
+# alias pip='pip3'       # /opt/homebrew/bin/pip3 also usr/bin
