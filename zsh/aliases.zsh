0e876c199717929fd5de7611961402e4aca305f0
zelong <zelong.guo@outlook.com>
2025-05-18 18:50:42 +0200

    first add fzf for zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,6 +4,7 @@ alias vi='nvim'
 alias ra='ranger --choosedir=$HOME/.rangerdir;LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
 alias t='tmux'
 alias g='lazygit'
+alias f='fzf'
 alias tn='tmux new -s zelong'
 # avoid using the following command as it'll lead to nesting ... 1 tmux session is already enough 
 alias t1='tmux new -s 1'
---
+++ zsh/fzf.zsh
@@ -0,0 +1,74 @@
+# https://github.com/nicksp/dotfiles/blob/main/zsh/fzf.zsh
+# Use fzf to search through the fd-results via fd (https://github.com/sharkdp/fd) to include hidden files (but exclude .git folders) and respect .gitignore
+# See more: https://github.com/junegunn/fzf/blob/master/ADVANCED.md
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --exclude .git'
+
+export FZF_COMPLETION_TRIGGER='\'
+export FZF_TMUX=1
+export FZF_TMUX_HEIGHT='80%'
+
+export FZF_COMPLETION_OPTS='--border --info=inline'
+
+FZF_COLORS="bg:-1,\
+bg+:-1,\
+fg:-1,\
+fg+:#ad9c8b,\
+hl:#d8a851,\
+hl+:#ceb250,\
+header:#ad9c8b,\
+border:#6B503C,\
+info:#6b503c,\
+prompt:#6b503c,\
+pointer:#6b503c,\
+marker:#7f61b3,\
+spinner:#6b503c"
+
+# https://vitormv.github.io/fzf-themes/
+export FZF_DEFAULT_OPTS="
+--height 60% \
+--border sharp \
+--layout reverse \
+--color '$FZF_COLORS' \
+--prompt '∷ ' \
+--pointer ▶ \
+--marker '✔ ' \
+--bind ctrl-k:down,ctrl-i:up
+--bind='ctrl-o:execute(code {})+abort' \
+--bind 'ctrl-/:change-preview-window(hidden|)' \
+--preview-window='border-sharp' \
+--info right"
+
+# Use fd to respect .gitignore, include hidden files and exclude `.git` folders
+# - The first argument to the function ($1) is the base path to start traversal
+_fzf_compgen_path() {
+  fd --hidden --exclude ".git" . "$1"
+}
+
+# Use fd to generate the list for directory completion
+_fzf_compgen_dir() {
+  fd --type d --hidden --exclude ".git" . "$1"
+}
+
+# Advanced customization of fzf options via _fzf_comprun function
+# - The first argument to the function is the name of the command.
+# - Make sure to pass the rest of the arguments to fzf.
+_fzf_comprun() {
+  local command=$1
+  shift
+
+  case "$command" in
+    # cd \<TAB>
+    # cd)      fzf --preview 'eza --tree --color=always {} | head -200'                        "$@" ;;
+    cd)      fd --type d --hidden                | fzf  "$@" ;;
+
+    # vim 
+    vim)     fd --type f --hidden --exclude .git | fzf --preview 'highlight -O ansi -l {}' "$@" ;;
+
+    # any_other_command \<TAB>
+    *)       fzf --preview 'highlight -O ansi -l {}' "$@" ;;
+  esac
+}
+
+
+# Set up fzf key bindings and fuzzy completion
+source <(fzf --zsh)
