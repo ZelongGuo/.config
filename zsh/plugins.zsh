5a0470dd65936d59119a86f06197e333b08d8714
zelong <zelong.guo@outlook.com>
2025-11-10 15:49:56 +0800

    add script for automatic linking .gmtversions, .tmux.conf and .shared files from .config/zsh to HOME

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,3 +1,4 @@
+# --------------------------- Zim & Zsh --------------------------- 
 export PLUG_DIR=$HOME/.zim
 if [[ ! -d $PLUG_DIR ]]; then
     curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
@@ -8,3 +9,22 @@ fi
 if ! cat $HOME/.zshrc | grep 'source ~/.config/zsh/zshrc' > /dev/null 2>&1; then
     echo "source ~/.config/zsh/zshrc" >> $HOME/.zshrc
 fi
+
+# --------------------------- gmtversions --------------------------- 
+local gmt_file=$HOME/.gmtversions
+if [[ ! -e $gmt_file ]]; then
+    ln -s $HOME/.config/zsh/.gmtversions $gmt_file
+fi
+
+# --------------------------- tmux conf --------------------------- 
+local tmux_conf=$HOME/.tmux.conf
+if [[ ! -e $tmux_conf ]]; then
+    ln -s $HOME/.config/zsh/.tmux_conf $tmux_conf
+fi
+
+# --------------------------- .shared --------------------------- 
+local shared_file=$HOME/.shared
+if [[ ! -e $shared_file ]]; then
+    ln -s $HOME/.config/zsh/.shared $shared_file
+fi
+
