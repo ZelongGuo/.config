3c3f854474a59474004aa12733484c22ff814c4f
zelong <zelong.guo@outlook.com>
2026-01-17 13:15:30 +0800

    exclude some folders for fzf searching

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -2,7 +2,7 @@
 # Use fzf to search through the fd-results via fd (https://github.com/sharkdp/fd) to include hidden files (but exclude .git folders) and respect .gitignore
 # See more: https://github.com/junegunn/fzf/blob/master/ADVANCED.md
 
-export FZF_DEFAULT_COMMAND='fd --hidden --exclude .git'
+export FZF_DEFAULT_COMMAND='fd --hidden --exclude .git'  # Updated after FD_EXCLUDES is defined below
 export FZF_COMPLETION_TRIGGER='\'
 export FZF_TMUX=1
 export FZF_TMUX_HEIGHT='80%'
@@ -53,57 +53,57 @@ export FZF_DEFAULT_OPTS="
 # --tiebreak='length,pathname' \
 # --info right"
 
-
-
-
-
 # Set up fzf key bindings and fuzzy completion
 source <(fzf --zsh)
 
 # ---------------------------------------------------------------------------------------
-# Disable the followings otherwise the completion would be slowed down
+# Centralized array of folder patterns to exclude from all fzf searches
+FD_EXCLUDES=(
+  --exclude '.cache'
+  --exclude '.conda'
+  --exclude '.git'
+  --exclude '.gmt'
+  --exclude '.local'
+  # --exclude '.config'
+  --exclude '__pycache__'
+  --exclude '.bundle'
+  --exclude '.themes'
+  --exclude '.codeintel'
+  --exclude 'python2.7'
+  --exclude 'python3.6'
+  --exclude 'Library'
+  --exclude 'opt'
+)
 
-# FD_EXCLUDES=(
-#   --exclude '.cache'
-#   --exclude '.conda'
-#   --exclude '.git'
-#   --exclude '.gmt'
-#   --exclude '.local'
-#   # --exclude '.config'
-#   --exclude '__pycache__'
-#   --exclude '.bundle'
-#   --exclude '.themes'
-#   --exclude '.codeintel'
-#   --exclude 'python2.7'
-#   --exclude 'python3.6'
-# )
+# Update FZF_DEFAULT_COMMAND to use FD_EXCLUDES for consistency
+export FZF_DEFAULT_COMMAND="fd --hidden ${FD_EXCLUDES[*]}"
 
 # Use fd to respect .gitignore, include hidden files and exclude `.git` folders
 # - The first argument to the function ($1) is the base path to start traversal
-# _fzf_compgen_path() {
-#   fd --hidden --exclude ".git" . "$1"
-  # fd --hidden "${FD_EXCLUDES[@]}" . "$1"
-# }
+# - This affects file completion (e.g., `vim \ <tab>`)
+_fzf_compgen_path() {
+  fd --hidden "${FD_EXCLUDES[@]}" . "$1"
+}
 
 
 # Use fd to generate the list for directory completion
-# _fzf_compgen_dir() {
-  # fd --type d --hidden --exclude ".git" . "$1"
-  # fd --type d --hidden "${FD_EXCLUDES[@]}" . "$1"
-# }
+# - This affects directory completion (e.g., `cd \ <tab>`)
+_fzf_compgen_dir() {
+  fd --type d --hidden "${FD_EXCLUDES[@]}" . "$1"
+}
 
 # # Advanced customization of fzf options via _fzf_comprun function
 # # - The first argument to the function is the name of the command.
 # # - Make sure to pass the rest of the arguments to fzf.
-# _fzf_comprun() {
-#   local command=$1
-#   shift
+_fzf_comprun() {
+  local command=$1
+  shift
 
-#   case "$command" in
-#     cd)           fzf --preview  'tree -C {} | head -200'   "$@" ;;
-#     # ssh)          fzf --preview 'dig {}'                   "$@" ;;
-#     vim)          fd --type f --hidden "${FD_EXCLUDES[@]}" | fzf --preview 'highlight -O ansi -l {} | head -200' "$@" ;;
-#     *)            fzf --preview 'highlight -O ansi -l {} | head -200'  "$@" ;;
-#   esac
-# }
+  case "$command" in
+    cd)           fzf --preview  'tree -C {} | head -200'   "$@" ;;
+    # ssh)          fzf --preview 'dig {}'                   "$@" ;;
+    vim)          fd --type f --hidden "${FD_EXCLUDES[@]}" | fzf --preview 'highlight -O ansi -l {} | head -200' "$@" ;;
+    *)            fzf --preview 'highlight -O ansi -l {} | head -200'  "$@" ;;
+  esac
+}
 
