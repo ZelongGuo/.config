ccbef6666c15bf976cfc88ca6cca948a21ce5759
zelong <zelong@zelong.guo>
2026-03-18 14:43:29 +0800

    update linux.zsh

--- zsh/os/linux.zsh
+++ zsh/os/linux.zsh
@@ -24,3 +24,28 @@
 # Uncomment and adjust for your proxy setup
 # export https_proxy=http://127.0.0.1:7890
 # export http_proxy=http://127.0.0.1:7890
+
+
+# -------------------------------------------------------------------
+# --- Git Proxy Auto Config (Mihomo Party / Clash) ---
+# Auto test if clash (mihomo party) is running, if so, configuring the proxy for git
+
+setup_git_proxy() {
+    # The local port is 14122
+    local local_port=14122
+    # The default clash port is 7890
+    local clash_port=7890
+
+    if lsof -i tcp:$local_port -sTCP:LISTEN >/dev/null 2>&1; then
+        git config --global http.proxy "http://127.0.0.1:$clash_port"
+        git config --global https.proxy "http://127.0.0.1:$clash_port"
+        # echo "[Git Proxy] enabled! (http://127.0.0.1:$clash_port)"
+    else
+        git config --global --unset http.proxy >/dev/null 2>&1
+        git config --global --unset https.proxy >/dev/null 2>&1
+        # echo "[Git Proxy] disabled!（clash is not running now）"
+    fi
+}
+
+setup_git_proxy
+
